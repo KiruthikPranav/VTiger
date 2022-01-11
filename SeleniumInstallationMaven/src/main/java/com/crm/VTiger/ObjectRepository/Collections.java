6bc5f48b38e50ddeeac9ecb2b61ee45b61810a31
KiruthikPranav <kiruthikpranavkarthigeyan@gmail.com>
2022-01-11 11:39:42 +0530

    Tuesday 11:39Am 1/11/2022

--- SeleniumInstallationMaven/src/main/java/com/crm/VTiger/ObjectRepository/Collections.java
+++ SeleniumInstallationMaven/src/main/java/com/crm/VTiger/ObjectRepository/Collections.java
@@ -1,12 +1,9 @@
 package com.crm.VTiger.ObjectRepository;
 
-import java.util.Date;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import com.fileutility.WebDriverUtilities;
 
 public class Collections {
