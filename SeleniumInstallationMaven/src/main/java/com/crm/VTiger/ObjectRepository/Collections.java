4eda2a98c8c980af8899b41c43e428c09c04e420
KiruthikPranav <kiruthikpranavkarthigeyan@gmail.com>
2022-01-11 11:36:40 +0530

    Tuesday 1/11/2022 11.35AM

--- SeleniumInstallationMaven/src/main/java/com/crm/VTiger/ObjectRepository/Collections.java
+++ SeleniumInstallationMaven/src/main/java/com/crm/VTiger/ObjectRepository/Collections.java
@@ -1,40 +1,43 @@
 package com.crm.VTiger.ObjectRepository;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.PriorityQueue;
+import java.util.Date;
 
-class Alpha{
-	
-}
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.fileutility.WebDriverUtilities;
 
 public class Collections {
 	public static void main(String[] args) {
+		WebDriverUtilities wb = new WebDriverUtilities();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://xdsoft.net/jqplugins/calendar-popup/");
 		
-		Alpha a = new Alpha();
 		
-		LinkedHashSet name = new LinkedHashSet();
+		WebElement element = driver.findElement(By.xpath("//div[@class='calendar-popup-composer calendar-popup-composer_main calendar-popu"
+				+ "p-composer_inline']//a[@class='calendar-po"
+				+ "pup-arrow calendar-popup-icon calendar-popup-arrow_next']"));
+		wb.explicittWaitElementVisibilty(driver, 15, element);
+		element.click();
+		WebElement mnth = driver.findElement(By.xpath("//div[@class='calendar-popup-composer calendar-popup"
+				+ "-composer_main calendar-popup-composer_inline']/div/div/div[1]"));
+		String month = mnth.getText();
+		WebElement yar = driver.findElement(By.xpath("//div[@class='calendar-popup-composer calendar-popup"
+				+ "-composer_main calendar-popup-composer_inline']/div/div/div[2]"));
+		String year = yar.getText();
+		//System.out.println(month);
+		String expMonth = "February";
+		String expYear = "2024";
+		while(!((month.equals(expMonth))&&(year.equals(expYear)))) {
+			element.click();
+			month = mnth.getText();
+			year = yar.getText();
+		}
+		System.out.println(year+" "+month);
 		
-		name.add("String");
-		name.add("String");
-		name.add("Alpha");
-		name.add(40);
-		name.add(2);
-		name.add(2);
-		name.add(5);
-		name.add(10);
-		name.add(-2);
-		name.add(1.0);
-		name.add(true);
-		name.add(false);
-		name.add(null);
-		name.add(null);
-		
-		Iterator itr = name.iterator();
-		
-		while(itr.hasNext()) {
-			System.out.println(itr.next());
-		}		
+		driver.quit();
 	}
 }
